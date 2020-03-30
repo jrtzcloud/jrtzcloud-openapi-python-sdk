c6ff01b1d8083815698579234ee9247d7ef87914
pantj <pantj@investoday.com.cn>
2020-03-30 18:55:06 +0800

    fix/demo

--- jrtzcloud-python-sdk-consensus/jrtzcloudsdkconsensus/v20200330/models.py
+++ jrtzcloud-python-sdk-consensus/jrtzcloudsdkconsensus/v20200330/models.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from common.abstract_model import AbstractModel
+from jrtzcloudsdkcore.abstract_model import AbstractModel
 
 
 class DataApiResponse(AbstractModel):
