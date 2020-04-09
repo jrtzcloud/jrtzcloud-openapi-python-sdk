9f1cc7b793424bb1e182d7cc8f0de3b29a2b0bb1
jrtzcloud <pantj@investoday.com.cn>
2020-04-09 16:58:42 +0800

    fix/blten patch_project

--- jrtzcloud-python-sdk-blten/jrtzcloudsdkblten/v20191119/models.py
+++ jrtzcloud-python-sdk-blten/jrtzcloudsdkblten/v20191119/models.py
@@ -35,7 +35,7 @@ class Patch(AbstractModel):
     """Patch 请求参数结构体
     """
     def __init__(self):
-        self.Op = None
+        self.Op = "replace"
         self.Path = None
         self.Value = None
 
@@ -55,5 +55,101 @@ class ProjectResponse(AbstractModel):
 
     def _deserialize(self, params):
         self.RequestId = params.get("RequestId")
-        self.Project = params.get("Project")
+        if params.get("Project") is not None:
+            self.Project = Project()
+            self.Project._deserialize(params.get("Project"))
+
+
+class Project(AbstractModel):
+    """模型项目响应体"""
+
+    def __init__(self):
+        self.Model = None
+        self.ProjectId = None
+        self.StartDate = None
+        self.StopDate = None
+        self.TasksLength = None
+
+    def _deserialize(self, params):
+        if params.get("Model") is not None:
+            self.Model = Model()
+            self.Model._deserialize(params.get("Model"))
+        self.ProjectId = params.get("ProjectId")
+        self.StartDate = params.get("StartDate")
+        self.StopDate = params.get("StopDate")
+        self.TasksLength = params.get("TasksLength")
+
+class Model(AbstractModel):
+    """模型响应体"""
+
+    def __init__(self):
+        self.AssetList = None
+        self.ConstrainList = None
+        self.BoundaryDict = None
+        self.OriginalExpRtnDict = None
+
+    def _deserialize(self, params):
+        self.AssetList = params.get("AssetList")
+        self.ConstrainList = params.get("ConstrainList")
+        if params.get("BoundaryDict") is not None:
+            self.BoundaryDict = BoundaryDict()
+            self.BoundaryDict._deserialize(params.get("BoundaryDict"))
+        if params.get("OriginalExpRtnDict") is not None:
+            self.OriginalExpRtnDict = OriginalExpRtnDict()
+            self.OriginalExpRtnDict._deserialize(params.get("OriginalExpRtnDict"))
+
+
+class OriginalExpRtnDict(AbstractModel):
+    """资产预期收益率字典"""
+
+    def __init__(self):
+        self.ABS_RETURN = None
+        self.ASHARE = None
+        self.CASH = None
+        self.CN_CREDIT = None
+        self.GLOBAL_DEBT = None
+        self.GOLD = None
+        self.HKSHARE = None
+        self.OIL = None
+        self.TREASURY = None
+        self.USSHARE = None
+
+    def _deserialize(self, params):
+        self.ABS_RETURN = params.get("ABS_RETURN")
+        self.ASHARE = params.get("ASHARE")
+        self.CASH = params.get("CASH")
+        self.CN_CREDIT = params.get("CN_CREDIT")
+        self.GLOBAL_DEBT = params.get("GLOBAL_DEBT")
+        self.GOLD = params.get("GOLD")
+        self.HKSHARE = params.get("HKSHARE")
+        self.OIL = params.get("OIL")
+        self.TREASURY = params.get("TREASURY")
+        self.USSHARE = params.get("USSHARE")
+
+class BoundaryDict(AbstractModel):
+    """资产持仓权重绝对值上下限字典"""
+
+    def __init__(self):
+        self.ABS_RETURN = None
+        self.ASHARE = None
+        self.CASH = None
+        self.CN_CREDIT = None
+        self.GLOBAL_DEBT = None
+        self.GOLD = None
+        self.HKSHARE = None
+        self.OIL = None
+        self.TREASURY = None
+        self.USSHARE = None
+
+    def _deserialize(self, params):
+        self.ABS_RETURN = params.get("ABS_RETURN")
+        self.ASHARE = params.get("ASHARE")
+        self.CASH = params.get("CASH")
+        self.CN_CREDIT = params.get("CN_CREDIT")
+        self.GLOBAL_DEBT = params.get("GLOBAL_DEBT")
+        self.GOLD = params.get("GOLD")
+        self.HKSHARE = params.get("HKSHARE")
+        self.OIL = params.get("OIL")
+        self.TREASURY = params.get("TREASURY")
+        self.USSHARE = params.get("USSHARE")
 
--- jrtzcloud-python-sdk-blten/tests/patch_project.py
+++ jrtzcloud-python-sdk-blten/tests/patch_project.py
@@ -30,80 +30,24 @@ try:
     req.Patch = []
     patch = models.Patch()
     patch.Op = "replace"
-    patch.Path = "/StopDate"
-    patch.Value = "2029-09-02"
+    patch.Path = "/StartDate"
+    patch.Value = "2020-03-08"
     patch2 = models.Patch()
     patch2.Op = "replace"
     patch2.Path = "/StopDate"
-    patch2.Value = "2029-09-02"
+    patch2.Value = "2029-09-03"
     req.Patch.append(patch)
     req.Patch.append(patch2)
 
-    # req.Patch = '''[
-    #     {"op": "replace", "path": "/Model", "value": {"AssetList": ["ASHARE", "USSHARE", "HKSHARE", "ABS_RETURN", "OIL", "GOLD", "TREASURY", "CN_CREDIT", "GLOBAL_DEBT", "CASH"], "OriginalExpRtnDict": {"ASHARE": 0.12, "USSHARE": 0.082, "HKSHARE": 0.082, "ABS_RETURN": 0.045, "OIL": 0.085, "GOLD": 0.06, "CN_CREDIT": 0.045, "TREASURY": 0.042, "GLOBAL_DEBT": 0.04, "CASH": 0.03}, "BoundaryDict": {"ASHARE": [0, 1.0], "USSHARE": [0, 1.0], "HKSHARE": [0, 1.0], "ABS_RETURN": [0, 0.1], "OIL": [0, 1.0], "GOLD": [0, 1.0], "TREASURY": [0.03, 1.0], "CN_CREDIT": [0, 1.0], "GLOBAL_DEBT": [0.0, 1.0], "CASH": [0.02, 1.0]}, "ConstrainList": [["ineq", "(0.3 * ASHARE) - USSHARE"]]}}, {"op": "replace", "path": "/StartDate", "value": "2019-09-01"}, {"op": "replace", "path": "/StopDate", "value": "2029-09-01"}
-    # ]
-    # '''
-
     # 这里还支持以标准json格式的string来赋值请求参数的方式。下面的代码跟上面的参数赋值是等效的。
-    # params = '''{
-    #     "Patch": [
-    #     {
-    #         "op": "replace",
-    #         "path": "/Model",
-    #         "value": {
-    #             "AssetList": ["ASHARE", "USSHARE", "HKSHARE", "ABS_RETURN", "OIL", "GOLD", "TREASURY", "CN_CREDIT", "GLOBAL_DEBT", "CASH"],
-    #             "OriginalExpRtnDict": {
-    #                 "ASHARE": 0.12,
-    #                 "USSHARE": 0.082,
-    #                 "HKSHARE": 0.082,
-    #                 "ABS_RETURN": 0.045,
-    #                 "OIL": 0.085,
-    #                 "GOLD": 0.06,
-    #                 "CN_CREDIT": 0.045,
-    #                 "TREASURY": 0.042,
-    #                 "GLOBAL_DEBT": 0.04,
-    #                 "CASH": 0.03
-    #             },
-    #             "BoundaryDict": {
-    #                 "ASHARE": [0, 1.0],
-    #                 "USSHARE": [0, 1.0],
-    #                 "HKSHARE": [0, 1.0],
-    #                 "ABS_RETURN": [0, 0.1],
-    #                 "OIL": [0, 1.0],
-    #                 "GOLD": [0, 1.0],
-    #                 "TREASURY": [0.03, 1.0],
-    #                 "CN_CREDIT": [0, 1.0],
-    #                 "GLOBAL_DEBT": [0.0, 1.0],
-    #                 "CASH": [0.02, 1.0]},
-    #                 "ConstrainList": [["ineq", "(0.3 * ASHARE) - USSHARE"]]
-    #             }
-    #         }
-    #     },
-    #     {
-    #         "op": "replace",
-    #         "path": "/StartDate",
-    #         "value": "2019-09-01"
-    #     },
-    #     {
-    #         "op": "replace",
-    #         "path": "/StopDate",
-    #         "value": "2029-09-01"
-    #     }
-    #     ]
-    # }
-    # '''
-
     params = '''{
-            "Patch": [
-            {   
-                "Op": "replace", 
-                "Path": "/StopDate", 
-                "Value": "2029-09-01"
-            }
-            ]
-        }
+        "Patch": [
+            {"Op": "replace", "Path": "/StartDate", "Value": "2020-03-09"},
+            {"Op": "replace", "Path": "/StopDate", "Value": "2029-09-01"}
+        ]
+    }
     '''
-    req.from_json_string(params)
+    # req.from_json_string(params)
 
     # 通过client对象调用方法发起请求。注意请求方法名与请求对象是对应的。
     # 返回的resp是一个Response类的实例，与请求对象对应。
@@ -114,7 +58,7 @@ try:
 
     # 也可以取出单个值。
     # 你可以通过官网接口文档或跳转到response对象的定义处查看返回字段的定义。
-    print(resp.Project)
+    print(resp.Project.Model.BoundaryDict.ASHARE)
 
 except JrtzCloudSDKException as err:
     print(err)
