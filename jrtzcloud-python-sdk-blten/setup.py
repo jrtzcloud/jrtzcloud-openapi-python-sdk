c70d17338874fa2120477c9c15663b3ad1c565b2
jrtzcloud <pantj@investoday.com.cn>
2020-04-09 21:10:51 +0800

    fix/core 1.0.2

--- jrtzcloud-python-sdk-blten/setup.py
+++ jrtzcloud-python-sdk-blten/setup.py
@@ -59,7 +59,7 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     platforms="any",
-    install_requires=["jrtzcloud-python-sdk-core>=1.0.1",],
+    install_requires=["jrtzcloud-python-sdk-core>=1.0.2"],
     classifiers=(
                 "Development Status :: 4 - Beta",
                 "Intended Audience :: Developers",
