44f2dfe24b26635f54f259bf7ef891deaccd5fce
jrtzcloud <pantj@investoday.com.cn>
2020-11-20 16:28:23 +0800

    fix/test

--- jrtzcloud-python-sdk-consensus/tests/test.py
+++ jrtzcloud-python-sdk-consensus/tests/test.py
@@ -3,13 +3,21 @@ import json
 import os
 
 from jrtzcloudsdkcore.exception.jrtzcloud_sdk_exception import JrtzCloudSDKException
+from jrtzcloudsdkcore.profile.client_profile import ClientProfile
+from jrtzcloudsdkcore.profile.http_profile import HttpProfile
+
 from jrtzcloudsdkconsensus.v20191119 import consensus_client, models
 
 USER = os.environ.get("JRTZCLOUD_LYZT_DEV_SECRET_ID")
 PWD = os.environ.get("JRTZCLOUD_LYZT_DEV_SECRET_KEY")
 
 try:
-    client = consensus_client.ConsensusClient(USER, PWD)
+    httpProfile = HttpProfile()
+    httpProfile.reqMethod = "GET"  # 请求方法(默认为post请求)
+    httpProfile.reqTimeout = 30  # 请求超时时间，单位为秒(默认60秒)
+    httpProfile.endpoint = "lyzt.dev.investoday.net"  # 指定接入地域域名(默认就近接入)
+    clientProfile = ClientProfile(httpProfile)
+    client = consensus_client.ConsensusClient(USER, PWD, profile=clientProfile)
 except JrtzCloudSDKException as err:
     print(err)
 
@@ -65,7 +73,10 @@ def fetch_jrtz_Anaem(client, models, start_date, end_date):
         resp = client.DescribeIndFrcstAnaem(req)
         # resp = client.DescribeEstBsc(req)
         resp_s = json.loads(resp.to_json_string())
-        print(resp_s['Page'])
+        print(resp_s['PageNo'])
+        da = resp_s['Data']
+        titles = resp_s['Fields']
+        value.append(titles)
         value.extend(resp_s['Data'])
         page += 1
         HasNextPage = resp_s['HasNextPage']
@@ -81,7 +92,7 @@ def dump_jrtz(client, models, date):
 
     data = pd.DataFrame(data)
     print('总数据长度=> ' + str(len(data)))
-    data = data[data['RptYr'] == int(str(date)[:4])]
+    # data = data[data['RptYr'] == int(str(date)[:4])]
     print('2019数据长度=> ' + str(len(data)))
     data.to_csv('{0}_{1}.csv'.format('jrtz', date), encoding='utf_8_sig')
 
